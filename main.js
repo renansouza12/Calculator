0363a6a8eb5bf3fbff389b1dbc96a9bd289915bb
RenanSouza <renansouza0122@gmail.com>
2023-03-19 13:21:15 -0300

    added  button CE | DEl | C | = and display the calculation

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const displayCurrent = document.querySelector('.display_current');
+const displayPrevious = document.querySelector('.display_previous');
 const buttons =document.querySelectorAll('button');
 
 buttons.forEach((btn)=>{
@@ -6,9 +7,21 @@ buttons.forEach((btn)=>{
        switch(btn.innerHTML){
         case 'CE':
             displayCurrent.innerHTML = '';
+            displayPrevious.innerHTML = '';
+            break;
+        case 'DEL':
+            displayCurrent.innerHTML = '';
+            break;
+        case 'C':
+            displayCurrent.innerHTML = displayCurrent.innerHTML.slice(0, -1);
+            break;
+        case '=':
+            displayPrevious.innerHTML = displayCurrent.innerHTML;
+            displayCurrent.innerHTML = eval(displayCurrent.innerHTML) ?? '';
             break;
         default:
-            displayCurrent.innerHTML += btn.innerHTML;
+            displayCurrent.innerHTML += btn.innerHTML;;
        }
+       console.log(displayCurrent.innerHTML);
     })
 })
