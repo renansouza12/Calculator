aea0b22d2ac3a23229fc22828532e089692c9dee
RenanSouza <renansouza0122@gmail.com>
2023-03-20 16:59:57 -0300

    effect click added

--- main.js
+++ main.js
@@ -16,25 +16,31 @@ buttons.forEach((btn)=>{
             displayCurrent.innerHTML = displayCurrent.innerHTML.slice(0, -1);
             break
         case '=':
-            validNumber();
+        validNumber();
             break;
         default:    
         displayCurrent.innerHTML.length > 15 ? alert("can't enter more 15 digits")  : displayCurrent.innerHTML += btn.innerHTML;
+       } 
 
-       }
-       
+       btn.style.borderBottom = '3px';
+       btn.style.borderRight = '1px';
+       btn.style.borderLeft = '1px';
+
+       setInterval(()=>{
+        btn.style.borderBottom = '';
+        btn.style.borderRight = '';
+        btn.style.borderLeft = '';
+       },200)
     })
 })
-
-
 
 function validNumber(){
     try{
         displayPrevious.innerHTML = displayCurrent.innerHTML;
-        displayCurrent.innerHTML = eval(displayCurrent.innerHTML) ?? '0';
+        displayCurrent.innerHTML = eval(displayCurrent.innerHTML) ?? '';
+           
     }catch{
         alert('Enter a valid number');
         displayPrevious.innerHTML = '';
-    }
-  
+    } 
 }
